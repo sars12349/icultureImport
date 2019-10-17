0a1f6146fb249581f3897325b694d0fe764be35d
sars12349 <sars12349@gmail.com>
2019-10-18 00:41:59 +0800

    連接調整，各頁面要做統一

--- includes/navigation.php
+++ includes/navigation.php
@@ -7,15 +7,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="i_culture_index.html">❤文創-審計新村</a>
+        <a class="navbar-brand" href="index.php">❤文創-審計新村</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 <!-- -----選單2及下拉選單----- -->
         <ul class="nav navbar-nav navbar-right">
-        <li><a href="iculture_about.html"><i class="fas fa-edit mr5"></i>關於我們</a></li>
-        <li><a href="i_culture_shop.html"><i class="fas fa-coffee mr5"></i>店家介紹</a></li>
-        <li><a href="iculture_traffic.html"><i class="fas fa-location-arrow mr5"></i>交通資訊</a></li>
+        <li><a href="about.php"><i class="fas fa-edit mr5"></i>關於我們</a></li>
+        <li><a href="shop.php"><i class="fas fa-coffee mr5"></i>店家介紹</a></li>
+        <li><a href="traffic.php"><i class="fas fa-location-arrow mr5"></i>交通資訊</a></li>
         </ul>
     </div>
     </div>
